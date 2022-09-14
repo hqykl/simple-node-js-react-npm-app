113e874dd7b5afe890f068489bc0e1aed9f0f02e
heqi1 <674228194@qq.com>
2022-09-14 09:27:46 +0800

    完成了整改

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code>想怎么改就怎么改啊212222
-        </p>
+        <p className="App-intro">简单测试一下，待更新</p>
+        <div>
+          备案号: <a href="https://beian.miit.gov.cn/">京ICP备2020039358号-1</a>
+        </div>
       </div>
     )
   }
